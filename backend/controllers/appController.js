4780c224b87ce268ed83ff617e5c474014d03ba0
zhenfa <oveu123@gmail.com>
2023-10-22 00:13:16 +0800

    [feat]-新增控制器、路由 issue:none

--- backend/controllers/appController.js
+++ backend/controllers/appController.js
@@ -1,10 +1,27 @@
-import createHttpError from "http-errors";
+/** 
+ * [登入] GET : /api/login
+ * @param { string } account  帳號
+ * @param { string } password 密碼
+ */
+export async function login(req, res) {
 
-
-export async function register (req, res) {
-    res.json({status:200, data:'register'});
 }
 
-export async function login(req, res){
-    
+/** 
+ * [取得文章列表] GET : /api/login 
+ * @param { number } index 文章頁籤，預設可為空
+ * 
+*/
+export async function getPosts(req, res) {
+
+}
+
+/** 
+ * [註冊] POST : /api/register 
+ * @param { string } account 帳號
+ * @param { string } password 密碼
+ * @param { string } sex 性別
+*/
+export async function register(req, res) {
+    res.status(200).json({status:200, data:"asd"});
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,25 @@
-
-import * as controller from '../controllers/appController';
-
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/appController');
 
-/* GET home page. */
-router.route('/test').get((req,res) => {
-  res.status(200).send("get sucess!!");
-})
+/** GET METHOD */
+router.route('/login').get(controller.login);
+router.route('/posts/:index').get(controller.getPosts);
+router.route('/post/:id').get(controller.getPost);
+router.route('/commemts/:id').get(controller.getComments);
 
+/** POST METHOD */
 router.route('/register').post(controller.register);
+router.route('/post').post(controller.createPost);
+router.route('/comment').post(controller.creatComment);
+
+/** PUT METHOD */
+router.route('/post/:id').put(controller.putPost);
+router.route('/comment/:id').put(controller.putComment);
+
+/** DELETE METHOD */
+router.route('/post/:id').delete(controller.deletePost);
+router.route('/comment/:id').delete(controller.deleteComment);
 
 
-export default router;
+module.exports = router;
